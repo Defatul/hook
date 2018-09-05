ce49a39634a4edf917afefcb3b193a80ef895740
Tolga Baki <defatul@gmail.com>
2018-09-05 16:07:04 +0300

    Start..

---
+++ class.hook.php
@@ -0,0 +1,35 @@
+<?php
+
+if(! class_exists('DEF_Hook') ){
+    class DEF_Hook {
+
+        /*!
+         * @var array
+         */
+        var $hooks = [];
+
+        function set_action($tag){
+            $this->hooks [$tag] = [];
+        }
+
+        function unset_action($tag){
+            unset( $this->hooks [$tag] );
+        }
+
+        function add_action($tag, $function, $priority = 10){
+            if( isset( $this->hooks [$tag] ) ){
+                $this->hooks [$tag] [$priority] = $function;
+            }else print ( 'Kanca Bağlantısı Yapılacak Fonksiyon Bulunamadı..' );
+        }
+
+        function do_action($tag, $args = NULL){
+            if( isset( $this->hooks [$tag] ) ){
+                foreach( $this->hooks [$tag] as $priority => $function ){
+                    $args = call_user_func( $function, $args );
+                }
+                return $args;
+            }else print ( 'Fonksiyon Bulunamadı..' );
+        }
+
+    }
+}else exit( '[DEF_Hook] CLASS OFF.' );
